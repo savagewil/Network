71a3fe3e88cbbd066583a90e5524a5edce728f8d
Savagewil <willsvg@gmail.com>
2017-09-18 14:45:33 -0400

    First Commit

---
+++ TraceRouteTest.py
@@ -0,0 +1,8 @@
+import subprocess
+processes = []
+count = 1
+processes.append(subprocess.Popen("tracert 129.21.1." + str(count % 255 + 1), stdin = subprocess.PIPE, stdout = subprocess.PIPE))
+
+
+while count < 256:
+    print processes[0].__dict__
