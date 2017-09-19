57d8338582260e3007dee13c26e88f2c410cacad
Savagewil <willsvg@gmail.com>
2017-09-19 08:36:45 -0400

    Uses Ping to scan Network for responsive IPs

--- TraceRouteTest.py
+++ TraceRouteTest.py
@@ -1,15 +1,95 @@
 import subprocess
 processes = []
-count = 1
-numberOfProcesses = 4
-for p in range(0, numberOfProcesses):
-    processes.append(subprocess.Popen("tracert 129.21.1." + str(count % 255 + 1), stdin = subprocess.PIPE, stdout = subprocess.PIPE))
-    count += 1
-while count < 256:
-    for p in range(0, len(processes)):
-        if not processes[p].poll() == None:
-            print processes[p].stdout.read()
-            processes[p] = processes.append(subprocess.Popen("tracert 129.21.1." + str(count % 255 + 1), stdin = subprocess.PIPE, stdout = subprocess.PIPE))
+count = 0
+numberOfProcesses = 10
+TestProcesses = True
+numberOfPings = 1
+IPs = {}
+Start_Octaves = [129, 21, 1, 1]
+End_Octaves = [129, 21, 2, 255]
+
+def generate_IP(count):
+    STRING = ""
+    for i in range(3,-1, -1):
+        STRING = "." + str(count % (End_Octaves[i] - Start_Octaves[i] + 1) + Start_Octaves[i]) + STRING
+        count = count // (End_Octaves[i] - Start_Octaves[i] + 1)
+    return STRING[1:]
+
+def findMyIP():
+    pass
+
+count = 0
+
+
+while count < numberOfProcesses:
+    try:
+        processes.append([subprocess.Popen("ping -n "+ str(numberOfPings) + " " + generate_IP(count),
+                                          stdin = subprocess.PIPE, stdout = subprocess.PIPE), generate_IP(count)])
+        if TestProcesses:
+            numberOfProcesses += 1
             count += 1
+    except Exception:
+        TestProcesses = False
+        numberOfProcesses = count
+print numberOfProcesses
+
+print (End_Octaves[0] - Start_Octaves[0] + 1) * (End_Octaves[1] - Start_Octaves[1] + 1) * (End_Octaves[2] - Start_Octaves[2] + 1) *(End_Octaves[3] - Start_Octaves[3] + 1)
+while count < (End_Octaves[0] - Start_Octaves[0] + 1) * (End_Octaves[1] - Start_Octaves[1] + 1) * \
+        (End_Octaves[2] - Start_Octaves[2] + 1) *(End_Octaves[3] - Start_Octaves[3] + 1):
+    for p in range(0, len(processes)):
+        if not processes[p][0].poll() == None:
+            print processes[p][1]
+            text = processes[p][0].stdout.read()
+            IPs[processes[p][1]] = (text.count("Request timed out.") != numberOfPings)
+            processes[p][0] = None
+            processes[p] = [subprocess.Popen("ping -n "+ str(numberOfPings) +" " + generate_IP(count),
+                                            stdin = subprocess.PIPE, stdout = subprocess.PIPE),\
+                           generate_IP(count)]
+            count += 1
+
+print "======================"
 for process in processes:
-    print process.stdout.read()
+    text = process[0].stdout.read()
+    IPs[process[1]] = (text.count("Request timed out.") != numberOfPings)
+
+for IP in IPs:
+    if IPs[IP]:
+        print IP
+
+"""129.21.1.155
+129.21.1.165
+129.21.1.23
+129.21.1.21
+129.21.1.20
+129.21.1.26
+129.21.1.25
+129.21.1.24
+129.21.1.29
+129.21.1.28
+129.21.1.164
+129.21.1.143
+129.21.1.30
+129.21.1.32
+129.21.1.35
+129.21.1.174
+129.21.1.175
+129.21.1.170
+129.21.1.169
+129.21.1.18
+129.21.1.19
+129.21.1.16
+129.21.1.17
+129.21.1.200
+129.21.1.90
+129.21.1.91
+129.21.1.98
+129.21.1.99
+129.21.1.114
+129.21.1.111
+129.21.1.183
+129.21.1.182
+129.21.1.104
+129.21.1.103
+129.21.1.101
+129.21.1.100
+129.21.1.166"""
